2f6d2cf85c28471e7b9cf4765a6a34af526a1598
SinusTangens <sinankammerer@gmail.com>
2026-03-26 23:16:13 +0100

    Anpassung der Operatorengewichtung

--- core/task_generator/Task.py
+++ core/task_generator/Task.py
@@ -43,7 +43,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(2, 2),
         max_depth=1,
         allowed_ops=["not", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "or": 1.0, "imp": 1.0, "xor": 1.0, "equiv": 1.0},
+        op_weights={"not": 0.75, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 
     (TaskType.DIRECT_INFERENCE, 2): DifficultySpec(
@@ -51,7 +51,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(2, 2),
         max_depth=2,
         allowed_ops=["not", "and", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "and": 0.75, "or": 1.0, "imp": 1.0, "xor": 0.5, "equiv": 0.5},
+        op_weights={"not": 0.75, "and": 1.0, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 
     (TaskType.DIRECT_INFERENCE, 3): DifficultySpec(
@@ -59,7 +59,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(2, 2),
         max_depth=2,
         allowed_ops=["not", "and", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "and": 0.75, "or": 1.0, "imp": 1.0, "xor": 1.0, "equiv": 1.0},
+        op_weights={"not": 0.75, "and": 1.0, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 
     (TaskType.DIRECT_INFERENCE, 4): DifficultySpec(
@@ -67,7 +67,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(2, 2),
         max_depth=3,
         allowed_ops=["not", "and", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "and": 0.75, "or": 1.0, "imp": 1.0, "xor": 1.0, "equiv": 1.0},
+        op_weights={"not": 0.75, "and": 1.0, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 
 
@@ -85,7 +85,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(5, 5),
         max_depth=2,
         allowed_ops=["not", "and", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "or": 1.0, "imp": 1.0, "xor": 0.75, "and": 1.0, "equiv": 0.75},
+        op_weights={"not": 0.75,"and": 1.0, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 
     (TaskType.CASE_SPLIT, 3): DifficultySpec(
@@ -93,7 +93,7 @@ DIFFICULTY_CONFIG: Dict[Tuple[TaskType, int], DifficultySpec] = {
         num_premises_range=(6, 6),
         max_depth=2,
         allowed_ops=["not", "and", "or", "imp", "xor", "equiv"],
-        op_weights={"not": 0.75, "or": 1.0, "imp": 1.0, "xor": 0.75, "and": 1.0, "equiv": 0.75},
+        op_weights={"not": 0.75, "and": 1.0, "or": 1.0, "imp": 1.0, "xor": 0.75, "equiv": 0.75},
     ),
 }
 
